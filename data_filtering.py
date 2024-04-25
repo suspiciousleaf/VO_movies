3e12bed77a321915d6047f1f53ffdd3a0bb11327
David <d.f.brooks01@gmail.com>
2024-04-25 17:56:08 +0200

    wrote str and repr methods for movie class

--- data_filtering.py
+++ data_filtering.py
@@ -1,7 +1,51 @@
 import json
 from pprint import pprint
 
+from movie import Movie
+
 with open("vo_showings.json", "r", encoding="utf8") as f:
     data = json.load(f)
 
-pprint(data[0])
+movie_list = []
+
+for item in data:
+    original_title = item["movie"]["originalTitle"]
+    french_title = item["movie"]["title"]
+    synopsis = item["movie"]["synopsis"]
+    image_poster = item["movie"]["poster"]["url"]
+    runtime = item["movie"]["runtime"]
+    genres = [genre["tag"].title() for genre in item["movie"]["genres"]]
+    languages = [language.title() for language in item["movie"]["languages"]]
+
+    cast = []
+    for cast_raw in item["movie"]["cast"]["edges"]:
+        cast_member = cast_raw["node"]["actor"]
+        name = " ".join([cast_member["firstName"], cast_member["lastName"]])
+        cast.append(name)
+
+    release_year = None
+    for release in item["movie"]["releases"]:
+        if release["__typename"] == "MovieRelease":
+            release_year = release["releaseDate"]["date"]
+            break
+    if not release_year:
+        release_year = str(item["data"]["productionYear"])
+
+    movie_list.append(
+        Movie(
+            original_title,
+            french_title,
+            image_poster,
+            runtime,
+            synopsis,
+            cast,
+            languages,
+            genres,
+            release_year,
+        )
+    )
+    # break
+
+for item in movie_list:
+    print(item)
+    print("\n")
--- movie.py
+++ movie.py
@@ -7,10 +7,11 @@ class Movie:
         original_title: str = "",
         french_title: str = "",
         image_poster: str = "",
-        runtime_min: int = 0,
+        runtime: str = "",
         synopsis: str = "",
-        cast: list[str] = [],
-        genres: list[str] = [],
+        cast: list[str] = [""],
+        languages: list[str] = [""],
+        genres: list[str] = [""],
         release_year: int = 0,
     ) -> None:
         # Original title
@@ -20,12 +21,20 @@ class Movie:
         # Poster image url
         self.image_poster = image_poster
         # Runtime
-        self.runtime = runtime_min
+        self.runtime = runtime
         # Synopsis
         self.synopsis = synopsis
         # Main cast
         self.cast = cast
+        # Language(s)
+        self.languages = languages
         # Genre(s)
         self.genres = genres
         # Release year
         self.release_year = release_year
+
+    def __str__(self):
+        return f"Original Title: {self.original_title} \nFrench Title: {self.french_title} \nPoster: {self.image_poster} \nRumtime: {self.runtime} \nSynopsis:{self.synopsis} \nCast: {self.cast} \nLanguages: {self.languages} \nGenre(s): {self.genres} \nRelease Year: {self.release_year}"
+
+    def __repr__(self):
+        return f"Movie('{self.original_title}', '{self.french_title}', '{self.image_poster}', '{self.runtime}', '{self.synopsis}', {self.cast}, {self.languages}, {self.genres}, {self.release_year})"
