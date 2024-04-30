4ff943fddd79b01face950aa2ecc201ec81c6422
David <d.f.brooks01@gmail.com>
2024-04-30 16:48:11 +0200

    Added functionality to insert new showings into database

--- showing.py
+++ showing.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 import hashlib
+from pprint import pprint
+
 from db_utilities import connect_to_database
+
+TABLE_NAME = "showtimes"
 
 
 class Showing:
-    #! Create method to return something that can be added to database
     def __init__(
         self,
         movie_id: str,
@@ -12,13 +15,14 @@ class Showing:
         start_time: datetime,
     ):
         self.movie_id = movie_id
-        self.start_time = start_time
         self.cinema_id = cinema_id
+        self.start_time = start_time
         self.hash_id = self.calculate_hash(
             self.movie_id, self.cinema_id, self.start_time
         )
 
-    # Hash function to create a single data point for each showing that can be compared with newly scraped showings, to identify showings not yet in the database
+    # Hash function to create a single data point for each showing that can be compared with newly scraped showings, to identify showings not yet in the database. @staticmethod used as it does not require 'self' to be passed.
+    @staticmethod
     def calculate_hash(movie_id: str, cinema_id: str, start_time: datetime) -> str:
         # Create a single string
         data = f"{movie_id}{cinema_id}{start_time}"
@@ -35,9 +39,9 @@ class ShowingsManager:
 
     @connect_to_database
     def retrieve_showings(self, db, cursor):
+        """Retrieve hash_id values for all showings in database"""
         try:
-            table_name = "showtimes"
-            query = f"SELECT hash_id FROM {table_name};"
+            query = f"SELECT hash_id FROM {TABLE_NAME};"
             cursor.execute(query)
             results = cursor.fetchall()
 
@@ -46,30 +50,46 @@ class ShowingsManager:
         except Exception as e:
             print(f"ShowingsManager.retrieve_showings: An error occurred: {str(e)}")
 
-    def create_showing(self, item, cinema_id):
+    def showing_already_in_database(self, hash_id):
+        """Check if hash_id is recognised. Return False if showing not in database"""
+        return hash_id in self.current_showings
+
+    def create_showing(self, item: dict, cinema_id: str) -> Showing:
         #! Find a way to pass cinema_id in from scraper - data not present in json. Could get by looping over each cinema, or maybe from url in response object
+        """Create Showing object(s) for each movie & showing object in scraped json, check if showing is already in database, and if not add to new_showings list for batch addition"""
+
         if item["showtimes"]["original"]:
             for showing in item["showtimes"]["original"]:
                 try:
+                    # Identify valuesfor new showing
                     movie_id = item["movie"]["id"]
                     date_str = showing["startsAt"]
                     start_time = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S")
-                    # Create new showing object, to create hash_id for comparison with current showings
+                    # Create new Showing object, to create hash_id for comparison with current showings
                     new_showing = Showing(movie_id, cinema_id, start_time)
-
-                    if new_showing.hash_id in self.current_showings:
-                        continue
-                    else:
+                    # Check if new showing is already in database by comparing hash_id, if not add to list to new showings to be added to database
+                    if not self.showing_already_in_database(new_showing.hash_id):
                         self.new_showings.append(new_showing)
                         self.current_showings.add(new_showing.hash_id)
                 except:
                     continue
 
+    @connect_to_database
+    def add_new_showings_to_database(self, db=None, cursor=None):
+        """Run this to add all new showings stored in self.new_showings to database"""
 
-# show_man = ShowingsManager()
-# print(show_man.current_showings)
+        # List of dicts of values for each new showing to be inserted into {TABLE_NAME} table
+        showing_values_list = [showing.__dict__ for showing in self.new_showings]
 
-#! Add functions to retrieve showings, compare newly scraped showings to those from database, and add new ones
-#! Maybe add cinema manager to retrieve cinemas from database for front end
-
-#! Method to add new_showings to database, same for new_movies
+        # All columns to be inserted into {TABLE_NAME} table
+        columns = ("movie_id", "cinema_id", "start_time", "hash_id")
+        # Create placeholders for values to be inserted
+        placeholders = ", ".join(f"%({key})s" for key in columns)
+        # Create INSERT query
+        insert_query = (
+            f"INSERT INTO {TABLE_NAME} ({', '.join(columns)}) VALUES ({placeholders});"
+        )
+        # Execute above query to insert new showings into database
+        cursor.executemany(insert_query, showing_values_list)
+        # Commit changes to database
+        db.commit()
