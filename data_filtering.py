6a0a5967ae98448bfd497a9b86e6ee534977282e
David <d.f.brooks01@gmail.com>
2024-04-30 19:13:07 +0200

    Modified to test new versions of movie and show manager

--- data_filtering.py
+++ data_filtering.py
@@ -8,28 +8,25 @@ from showing import ShowingsManager
 with open("vo_showings.json", "r", encoding="utf8") as f:
     data = json.load(f)
 
-# all_movies = {}
-
 movie_man = MovieManager()
 show_man = ShowingsManager()
 
 
 # Loop through all scraped movie/showings in json. Each item relates to a single cinema on a single day,and will contain one movie, and one or more showings for that movie
 for item in data:
+    # Using dummy cinema ID for now
     cinema_id = "P8110"
-    # Set movie_id
-    movie_id = item["movie"]["id"]
-    # Check if movie is in database, if not then add it
-    if not movie_man.movie_already_in_database(movie_id):
-        movie_man.add_new_movie(item)
-    # Check if showings are in database, if not then add them to show_man.new_showings list
-    show_man.create_showing(item, cinema_id)
-    break
+    # Process movie
+    movie_man.process_movie(item)
+    # Process showing
+    show_man.process_showing(item, cinema_id)
+    # break
 
-
+# print(len(movie_man.new_movies))
 # print(show_man.new_showings)
 # pprint(show_man.current_showings)
 # break
+movie_man.add_new_movies_to_database()
 show_man.add_new_showings_to_database()
 
 # print(f"Total new movies added: {len(movie_man.new_movies)}")
