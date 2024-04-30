907ddfcda4b76a2f7db76e65f207c6588606c9ce
David <d.f.brooks01@gmail.com>
2024-04-30 16:51:52 +0200

    Changed to test new showing database capability

--- data_filtering.py
+++ data_filtering.py
@@ -14,22 +14,23 @@ movie_man = MovieManager()
 show_man = ShowingsManager()
 
 
-# Loop through all scraped movie/showings in json
+# Loop through all scraped movie/showings in json. Each item relates to a single cinema on a single day,and will contain one movie, and one or more showings for that movie
 for item in data:
     cinema_id = "P8110"
     # Set movie_id
     movie_id = item["movie"]["id"]
     # Check if movie is in database, if not then add it
-    if not movie_man.verify_movie_id_in_current_movies(movie_id):
+    if not movie_man.movie_already_in_database(movie_id):
         movie_man.add_new_movie(item)
-    # Check if showings are in database, if not then add them
+    # Check if showings are in database, if not then add them to show_man.new_showings list
     show_man.create_showing(item, cinema_id)
+    break
 
 
 # print(show_man.new_showings)
-pprint(show_man.current_showings)
+# pprint(show_man.current_showings)
 # break
-
+show_man.add_new_showings_to_database()
 
 # print(f"Total new movies added: {len(movie_man.new_movies)}")
 
