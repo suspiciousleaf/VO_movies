f6804ccab4e8c315f9f953da6686f80d874f5103
David <d.f.brooks01@gmail.com>
2024-05-07 19:42:25 +0200

    Added POST endpoint to add cinema to database

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, HTTPException
 
 from search import Search
+from cinema import CinemaManager
+from fastapi_cinema_model import CinemaModel
 
 app = FastAPI()
 search = Search()
@@ -18,3 +20,17 @@ def find_showings(towns: str | None = Query(default=None, min_length=3)) -> list
     else:
         towns = towns.split(",")
         return search.search(towns)
+
+
+@app.post("/add_cinema", status_code=201)
+def create_cinema(cinema: CinemaModel):
+    cinema_man = CinemaManager()
+    response = cinema_man.add_cinema_to_database(cinema.__dict__)
+    response["payload"] = cinema
+    if response["ok"]:
+        return response
+    else:
+        raise HTTPException(status_code=response["code"], detail=response["info"])
+
+
+# TODO End point to insert new cinema into database, maybe end point to modify cinemas, add info, and end point to delete cinemas á la CRUD. Maybe add additional scraper to get cinema details
