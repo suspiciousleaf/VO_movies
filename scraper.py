eda4e09ec44fec6e3e345ca6db8e237d5ed18c19
David <d.f.brooks01@gmail.com>
2024-05-12 16:12:37 +0200

    Added option to process local saved results, or scrape new ones

--- scraper.py
+++ scraper.py
@@ -94,7 +94,13 @@ class Scraper:
 
 
 class ScraperManager:
-    def __init__(self, start_day, end_day, save_raw_json_data=False):
+    def __init__(
+        self,
+        start_day,
+        end_day,
+        save_raw_json_data=False,
+        local_data_filename: str | None = None,
+    ):
         """
         Initialize a ScraperManager object.
 
@@ -103,7 +109,13 @@ class ScraperManager:
             end_day (int): The ending day for scraping.
             save_raw_json_data (bool, optional): Whether to save raw JSON data. Defaults to False.
         """
-        self.all_scraped_json_data = []
+        self.all_scraped_json_data = {}
+        self.local_data_filename = local_data_filename
+        if self.local_data_filename is not None and save_raw_json_data:
+            raise Exception(
+                "save_raw_json_data and local_data_filename are mutually exclusive: data can only be imported or exported, not both."
+            )
+
         self.run_scrapers(start_day, end_day)
         # print(movie_man)
         movie_man.add_new_movies_to_database()
@@ -123,15 +135,31 @@ class ScraperManager:
             start_day (int): The starting day for scraping. Today is day 0.
             end_day (int): The ending day for scraping.
         """
-        with requests.Session() as session:
-            for cinema in tqdm(cinema_man.cinema_ids, unit="Cinema"):
-                scraper = Scraper(session, cinema, start_day, end_day)
-                data = scraper.return_data()
-                for showing in data:
-                    # Process movie
-                    movie_man.process_movie(showing)
-                    # Process showing
-                    show_man.process_showing(showing, cinema)
+        if self.local_data_filename is None:
+            with requests.Session() as session:
+                for cinema in tqdm(cinema_man.cinema_ids, unit="Cinema"):
+                    scraper = Scraper(session, cinema, start_day, end_day)
+                    data = scraper.return_data()
+                    self.all_scraped_json_data[cinema] = data
+                    self.process_data(cinema, data)
+        else:
+            with open(
+                f"raw_data/{self.local_data_filename}", "r", encoding="utf8"
+            ) as f:
+                local_data = json.load(f)
+            for cinema, data in local_data.items():
+                self.process_data(cinema, data)
+
+    @staticmethod
+    def process_data(cinema, data):
+        for showing in data:
+            try:
+                # Process movie
+                movie_man.process_movie(showing)
+                # Process showing
+                show_man.process_showing(showing, cinema)
+            except Exception as e:
+                print(f"Movie could not be processed: {e}")
 
     def save_raw_data(self):
         """Save raw JSON data to a file."""
@@ -139,12 +167,14 @@ class ScraperManager:
         formatted_time = current_time.strftime("%Y-%m-%d_%H-%M-%S")
         file_name = f"raw_data_{formatted_time}.json"
 
-        with open(file_name, "w", encoding="utf8") as f:
+        with open(f"raw_data/{file_name}", "w", encoding="utf8") as f:
             json.dump(self.all_scraped_json_data, f, ensure_ascii=False)
 
 
 if __name__ == "__main__":
     t0 = time.perf_counter()
-    scraper_man = ScraperManager(1, 14, save_raw_json_data=True)
+    scraper_man = ScraperManager(
+        1, 2, local_data_filename="raw_data_2024-05-12_16-08-45.json"
+    )
 
     print(f"Time taken: {time.perf_counter() - t0:.2f}s")
