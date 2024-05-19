54599439d6099b56c82bff81f2702b2c7563c022
David <d.f.brooks01@gmail.com>
2024-05-19 17:35:23 +0200

    Updated import paths

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 
 from routers.cinema_router import router as cinema_router
-from routers.showings_router import router as showings_router
+from routers.search_router import router as search_router
 from routers.limiter import limiter
 from search import Search
 from logging import getLogger
@@ -36,7 +36,7 @@ app.state.logger = logger
 
 # Add routers
 app.include_router(cinema_router)
-app.include_router(showings_router)
+app.include_router(search_router)
 
 # Add custom exception handler
 app.add_exception_handler(RequestValidationError, validation_exception_handler)
@@ -51,6 +51,3 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 @limiter.limit("1/second")
 def ping(request: Request) -> str:
     return "The server is running."
-
-
-# TODO End point to modify cinemas, add info, and end point to delete cinemas á la CRUD. Maybe add additional scraper to get cinema details
