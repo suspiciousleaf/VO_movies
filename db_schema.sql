e0e81ef63af6d8ad02d123dd51da7af0d4965db2
David <d.f.brooks01@gmail.com>
2024-05-22 21:58:53 +0200

    Fixed typo

--- db_schema.sql
+++ db_schema.sql
@@ -16,9 +16,7 @@ CREATE TABLE movies (
     poster_hi_res VARCHAR(255),
     poster_lo_res VARCHAR(255),
     tagline VARCHAR(255),
-    tmdb_id INT UNSIGNED,
-
-);
+    tmdb_id INT UNSIGNED);
 
 -- Create cinemas table
 CREATE TABLE cinemas (
@@ -27,8 +25,7 @@ CREATE TABLE cinemas (
     `address` VARCHAR(255),
     info VARCHAR(255),
     gps POINT,
-    town VARCHAR(191)
-);
+    town VARCHAR(191));
 
 -- Create showtimes table
 -- hash_id is used to compare showings in database with newly scrapes showings to identify unknown ones, using SHA256
@@ -40,5 +37,4 @@ CREATE TABLE showtimes (
     hash_id CHAR(64),
     CONSTRAINT fk_movie_id FOREIGN KEY (movie_id) REFERENCES movies(movie_id),
     CONSTRAINT fk_cinema_id FOREIGN KEY (cinema_id) REFERENCES cinemas(cinema_id),
-    CONSTRAINT unique_hash_id UNIQUE (hash_id)
-);
+    CONSTRAINT unique_hash_id UNIQUE (hash_id));
