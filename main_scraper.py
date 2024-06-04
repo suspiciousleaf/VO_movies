04817cc8fab50f9aa409b0e5a2db2ecea604a1c4
David <d.f.brooks01@gmail.com>
2024-06-04 18:46:35 +0200

    Modified main_scraper to try with cron job

--- main_scraper.py
+++ main_scraper.py
@@ -6,21 +6,27 @@ from logs.setup_logger import setup_logging
 
 
 if __name__ == "__main__":
-    t0 = time.perf_counter()
-
-    logger = getLogger(__name__)
-    setup_logging()
-    # logger.setLevel("DEBUG")
-
     try:
-        scraper_man = ScraperManager(
-            start=1,
-            end=15,
-            # save_raw_json_data=True,
-            # local_data_filename="raw_data_2024-05-12_16-57-25.json",
-            logger=logger,
-        )
-    except Exception as e:
-        logger.exception(e)
+        t0 = time.perf_counter()
 
-    logger.info(f"Time taken: {time.perf_counter() - t0:.2f}s")
+        logger = getLogger(__name__)
+        setup_logging()
+
+        start = 10
+        end = 15
+
+        try:
+            scraper_man = ScraperManager(
+                start=start,
+                end=end,
+                save_raw_json_data=False,
+                logger=logger,
+            )
+            logger.info(
+                f"Time taken: {time.perf_counter() - t0:.2f}s, days = {start}-{end}"
+            )
+        except Exception as e:
+            logger.exception(e)
+
+    except:
+        pass
