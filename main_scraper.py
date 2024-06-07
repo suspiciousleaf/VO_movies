64ceb82026b54402c9c8850ecb4c770f48bc9e8a
David <d.f.brooks01@gmail.com>
2024-06-07 22:09:12 +0200

    Changed logging info

--- main_scraper.py
+++ main_scraper.py
@@ -1,4 +1,5 @@
 import time
+import datetime
 from logging import getLogger
 
 from scraper import ScraperManager
@@ -12,18 +13,26 @@ if __name__ == "__main__":
         logger = getLogger(__name__)
         setup_logging()
 
-        start = 10
+        start = 11
         end = 15
+
+        # Calculate the dates
+        today = datetime.date.today()
+        start_date = today + datetime.timedelta(days=start)
+        end_date = today + datetime.timedelta(days=end)
+
+        # Format the dates
+        start_date_str = start_date.strftime("%d-%m-%Y")
+        end_date_str = end_date.strftime("%d-%m-%Y")
 
         try:
             scraper_man = ScraperManager(
-                start=start,
-                end=end,
-                save_raw_json_data=False,
+                start_day=start,
+                end_day=end,
                 logger=logger,
             )
             logger.info(
-                f"Time taken: {time.perf_counter() - t0:.2f}s, days = {start}-{end}"
+                f"Ran scraper. Time taken: {time.perf_counter() - t0:.2f}s, dates = {start_date_str} - {end_date_str}"
             )
         except Exception as e:
             logger.exception(e)
