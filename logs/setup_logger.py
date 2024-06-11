b68b7094c13fcbd12aad400e627fb81f17371cf4
David <d.f.brooks01@gmail.com>
2024-06-11 20:00:10 +0200

    Added remote logging handler to logging config

--- logs/setup_logger.py
+++ logs/setup_logger.py
@@ -1,11 +1,16 @@
 import logging.config
 import json
 import atexit
+from os import getenv
 
 
 def setup_logging():
     with open("logs/logging_config.json", "r") as f:
         config = json.load(f)
+    config["handlers"]["syslog"]["address"] = (
+        getenv("PT_HOST"),
+        getenv("PT_PORT"),
+    )
     logging.config.dictConfig(config)
     queue_handler = logging.getHandlerByName("queue_handler")
     if queue_handler is not None:
