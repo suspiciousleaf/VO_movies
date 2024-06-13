3089447856810257ddab021b656d701245960d54
David <d.f.brooks01@gmail.com>
2024-06-13 18:27:35 +0200

    Removed redundant try/except blcok

--- main_scraper.py
+++ main_scraper.py
@@ -37,17 +37,13 @@ if __name__ == "__main__":
         start_date_str = start_date.strftime("%d-%m-%Y")
         end_date_str = end_date.strftime("%d-%m-%Y")
 
-        try:
-            scraper_man = ScraperManager(
-                start_day=start,
-                end_day=end,
-                logger=logger,
-            )
-            logger.info(
-                f"Ran scraper. Time taken: {time.perf_counter() - t0:.2f}s, dates: {start_date_str} - {end_date_str}"
-            )
-        except Exception as e:
-            logger.exception(e)
-
-    except:
-        pass
+        scraper_man = ScraperManager(
+            start_day=start,
+            end_day=end,
+            logger=logger,
+        )
+        logger.info(
+            f"Ran scraper. Time taken: {time.perf_counter() - t0:.2f}s, dates: {start_date_str} - {end_date_str}"
+        )
+    except Exception as e:
+        logger.exception(e)
